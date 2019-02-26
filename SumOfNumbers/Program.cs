e5c3c672d0077856cc0f5afe8169adafddaaceb9
Joel-Crunk <joel.crunk@outlook.com>
2019-02-26 13:44:22 -0500

    This commit will give the sum of up to ten different numbers

--- SumOfNumbers/Program.cs
+++ SumOfNumbers/Program.cs
@@ -8,8 +8,29 @@ namespace SumOfNumbers
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
+            int sum = 0;
+           
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter a number to be averaged\nEnter 0 if no more numbers:   ");
+             
+                int number = int.Parse(Console.ReadLine());
+
+                if (number == 0)
+                {
+                    i = 10;
+                }
+                sum = sum + number;
+
+                Console.WriteLine($"\nSubtotal: {sum}\n\n");
+                
+            }
+            Console.WriteLine($"The sum of these numbers is {sum}");
+
+            Console.ReadLine();
+
         }
     }
 }
